311104d873726a21cc57dae0935f72b3783ca6bf
Сергей <korkin.sergei@mail.ru>
2024-12-24 20:50:27 +0300

    1.1.4 решение

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,80 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        Session session = Util.getSession();
 
+        session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS Users(" +
+                "id BIGSERIAL NOT NULL PRIMARY KEY," +
+                "name VARCHAR(30) NOT NULL," +
+                "lastname VARCHAR(30) NOT NULL," +
+                "age INTEGER NOT NULL);").addEntity(User.class).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = Util.getSession();
 
+        session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS Users").addEntity(User.class).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Session session = Util.getSession();
 
+        session.beginTransaction();
+        session.persist(new User(name, lastName, age));
+
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
+        Session session = Util.getSession();
 
+        session.beginTransaction();
+
+        session.delete(session.get(User.class, id));
+
+        session.getTransaction().commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Session session = Util.getSession();
+        session.beginTransaction();
+        List<User> users = session.createQuery("From User", User.class).list();
+        session.close();
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = Util.getSession();
+        session.beginTransaction();
+        session.createQuery("DELETE FROM User").executeUpdate();
+        session.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,16 @@
 package jm.task.core.jdbc.util;
 
 import java.io.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -13,6 +19,7 @@ public class Util {
     String username;
     String password;
     Connection connection;
+    static SessionFactory sessionFactory;
 
     public Util() throws IOException, SQLException {
         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/database.properties"));
@@ -25,6 +32,12 @@ public class Util {
 
     public Connection getConnection() { return this.connection; }
 
-
+    public static Session getSession() {
+        if (sessionFactory == null){
+            Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        return sessionFactory.getCurrentSession();
+    }
 
 }
